e49847e67e4e1678bbb34afd858f3982a8289dff
vedadehhc <51378145+vedadehhc@users.noreply.github.com>
2021-11-07 12:03:19 -0500

    make screen a class

--- screen.h
+++ screen.h
@@ -1,11 +1,25 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
-namespace screen {
-	//Screen dimension constants
-	const int SCREEN_WIDTH = 1280;
-	const int SCREEN_HEIGHT = 720;
+class Screen {
+private:
+	// screen dimensions
+	int width, height;
 	
-}
+public:
+	Screen(int screenWidth, int screenHeight) {
+		width = screenWidth;
+		height = screenHeight;
+	}
+	
+	// getters
+	int getWidth() {
+		return width;
+	}
+	
+	int getHeight() {
+		return height;
+	}
+};
 
 #endif
