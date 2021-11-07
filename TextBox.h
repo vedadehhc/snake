d4be5d31f9aa1816189866aa82d257a05dcdcdca
vedadehhc <51378145+vedadehhc@users.noreply.github.com>
2021-11-07 17:31:45 -0500

    added fields + methods to TextBox

--- TextBox.h
+++ TextBox.h
@@ -1,7 +1,35 @@
 #ifndef TEXTBOX_h
 #define TEXTBOX_h
 
+#include <string>
+#include <SDL_ttf.h>
+#include <SDL.h>
+
+
 class TextBox {
+private:
+	int width, height;
+	std::string text;
+	SDL_Texture* gTextTexture = NULL;
+	
+public:
+	TextBox(std::string s) : text(s) {
+		
+	}
+	
+	bool free() {
+		
+	}
+	
+	
+	void setText(std::string s) {
+		text = s;
+		SDL_Surface* textSurface = TTF_RenderText_Blended_Wrapped( gFont, text.c_str(), color, 200 );
+	}
+	
+	std::string getText() {
+		return text;
+	}
 	
 }
 
